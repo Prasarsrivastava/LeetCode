11963d7f83e053480c0d8f76c78df67a057f7e1a
Prasarsrivastava <143502493+Prasarsrivastava@users.noreply.github.com>
2025-11-19 20:37:08 +0530

    Time: 0 ms (100%), Space: 42.5 MB (5.61%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int trailingZeroes(int n) {
+        int i,res=0;
+        for(i=5;i<=n;i=i*5)
+        {
+            res=res+n/i;
+            
+        }
+      return res;  
+    }
+}
